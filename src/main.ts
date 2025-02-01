001fbbb7811ffa936970395ee9a9cf59f125a181
armandoC1 <hr24006@clases.edu.sv>
2025-02-01 02:18:37 -0600

    cors2

--- src/main.ts
+++ src/main.ts
@@ -5,9 +5,9 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: '*', 
+    origin: 'https://polinomios-app.vercel.app/', 
     methods: ['GET', 'POST', 'PUT', 'DELETE'], 
-    allowedHeaders: ['Content-Type', 'Authorization'],
+    allowedHeaders: ['Content-Type'],
   });
 
   await app.listen(3000); 
