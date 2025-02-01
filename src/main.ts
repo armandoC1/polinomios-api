f4d33ea28562e8158a080e9aabc4511ac9002cee
armandoC1 <hr24006@clases.edu.sv>
2025-02-01 01:52:16 -0600

    cors

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,14 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  app.enableCors({
+    origin: '*', 
+    methods: ['GET', 'POST', 'PUT', 'DELETE'], 
+    allowedHeaders: ['Content-Type', 'Authorization'],
+  });
+
+  await app.listen(3000); 
+  console.log(`🚀 Servidor corriendo en http://localhost:3000`);
 }
 bootstrap();
